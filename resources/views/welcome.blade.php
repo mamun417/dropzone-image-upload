b65659420cd43fb53045db6d4706e934fe4b8a54
Abdullah Al Mamun <abdullah.ssc13@gmail.com>
2021-01-21 18:16:14 +0600

    get file and send server for upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,11 @@
     <title>PHP Dropzone File Upload on Button Click Example</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
 </head>
 <body>
 
@@ -13,7 +16,9 @@
     <div class="row">
         <div class="col-md-12">
             <h2>PHP Dropzone File Upload on Button Click Example</h2>
-            <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data" class="dropzone" id="dropzone">
+            <form name="form" action="{{ route('upload') }}" method="post" enctype="multipart/form-data"
+                  class="dropzone" id="dropzone">
+
                 @csrf
                 <input type="text" name="name" value="">
                 <input type="email" name="email" value="">
@@ -31,7 +36,7 @@
 
 <script type="text/javascript">
 
-    Dropzone.options.dropzone= {
+    Dropzone.options.dropzone = {
         url: '{{ route('upload') }}',
         autoProcessQueue: false,
         uploadMultiple: true,
@@ -40,27 +45,53 @@
         maxFilesize: 1,
         acceptedFiles: 'image/*',
         addRemoveLinks: true,
-        init: function() {
-            dzClosure = this; // Makes sure that 'this' is understood inside the functions below.
+        init: function () {
+            dropZone = this; // Makes sure that 'this' is understood inside the functions below.
+
+            // $('#uploadFile').click(function () {
+            //     dropZone.processQueue();
+            // })
 
             // for Dropzone to process the queue (instead of default form behavior):
-            document.getElementById("uploadFile").addEventListener("click", function(e) {
-                // Make sure that the form isn't actually being sent.
-                e.preventDefault();
-                e.stopPropagation();
-                dzClosure.processQueue();
-            });
+            // document.getElementById("uploadFile").addEventListener("click", function(e) {
+            //     // Make sure that the form isn't actually being sent.
+            //     // e.preventDefault();
+            //     // e.stopPropagation();
+            // });
 
             //send all the form data along with the files:
             // this.on("sending", function(data, xhr, formData) {
             //     formData.append("firstname", 'Abdullah Al Mamun');
             // });
 
-            // this.on("error", function(errorMessage) {
+            // this.on("success", function (errorMessage) {
             //     console.log(errorMessage)
+            //     console.log('rrorororor')
             // });
         }
     }
+
+    $('#uploadFile').click(function () {
+
+        let form = document.getElementById('dropzone')
+
+        console.log(dropZone.files)
+
+        let fd = new FormData(form);
+        fd.append('name', 'lll')
+        fd.append('files[]', dropZone.files[0])
+        fd.append('files[]', dropZone.files[1])
+
+        axios.post('{{ route('upload') }}', fd)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => {
+                $.each(error.response.data.errors, function (k, error) {
+                    console.log(error[0])
+                });
+            })
+    })
 
 </script>
 
